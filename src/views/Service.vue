2e5c281852bfda1230bf89a31829700b3e767f54
HajMo <haj-mo@protonmail.com>
2021-04-22 21:53:19 +0200

    Change page name ~.~

--- src/views/Service.vue
+++ src/views/Service.vue
@@ -2,7 +2,7 @@
     <div class="posts">
         <div class="posts-title">
             <div class="overlay"></div>
-            <h2>{{ $t('blog') }}</h2>
+            <h2>{{ $t('services') }}</h2>
         </div>
 
         <vs-row vs-w="12">
